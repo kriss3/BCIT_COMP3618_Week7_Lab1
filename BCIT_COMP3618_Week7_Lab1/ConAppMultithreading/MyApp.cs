2d46e8e254bd75a437696671d498c3adcdedd2c6
Krzysztof Szczurowski <kszczurowski@absolute.com>
2018-02-23 23:32:03 -0800

    Adding implementation of Counting method and instantiating Thread class to show the usage of it.

--- BCIT_COMP3618_Week7_Lab1/ConAppMultithreading/MyApp.cs
+++ BCIT_COMP3618_Week7_Lab1/ConAppMultithreading/MyApp.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
+using static System.Console;
 
 namespace ConAppMultithreading
 {
+    /// <summary>
+    /// Krzysztof Szczurowski
+    /// Week 7 Lab 1
+    /// Repo: https://github.com/kriss3/BCIT_COMP3618_Week7_Lab1.git
+    /// </summary>
     class MyApp
     {
         static void Main(string[] args)
         {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+            first.Start();
+            second.Start();
+            first.Join();
+            second.Join();
+            Read();
+        }
+
+        private static void Counting()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                WriteLine($"Count: {i} - Thread: {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+            }
         }
     }
 }
